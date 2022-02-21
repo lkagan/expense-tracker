76dd19f7e84c9175cf057182c2226e4c5f7a01f5
Larry Kagan <larry@superiocity.com>
2022-02-21 11:16:27 -0500

    Show/hide form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+    const [showForm, setShowForm] = useState(false);
+
     const saveExpenseHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
@@ -10,11 +12,27 @@ const NewExpense = (props) => {
         }
 
         props.onAddExpense(expenseData);
+        setShowForm(false);
     };
-    
+
+    const showFormHandler = () => {
+        setShowForm(true);
+    }
+
+    const hideFormHandler = () => {
+        setShowForm(false);
+    }
+
     return (
         <div className={"new-expense"}>
-            <ExpenseForm onSaveExpenseData={saveExpenseHandler} />
+            {showForm && (
+                <ExpenseForm
+                    onHideForm={hideFormHandler}
+                    onSaveExpenseData={saveExpenseHandler}/>
+            )}
+            {!showForm && (
+                <button onClick={showFormHandler}>Add New Expense</button>
+            )}
         </div>
     );
 }
