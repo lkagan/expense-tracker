1ec2dcfee5fc08dfbad343d68a5cf6808dfdaa2a
Larry Kagan <larry@superiocity.com>
2022-02-22 11:41:54 -0500

    Expenses chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,7 @@
+import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 import {useState} from "react";
 import ExpensesList from "./ExpensesList";
 
@@ -18,6 +20,7 @@ export default function Expenses(props) {
         <div>
             <Card className={"expenses"}>
             <ExpensesFilter onYearChange={onYearChangeHandler} selected={filterYear}/>
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
